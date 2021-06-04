3a53465131f8f44e4f3786b1510f05d55587040b
daanworks <daniel.babinszky@gmail.com>
2021-06-04 14:59:28 +0200

    time is over, not much work finished :(

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,3 +29,19 @@ const Logo = styled.img`
   margin-left: 10px;
   cursor: pointer;
 `
+
+const NewGame = styled.div`
+  background: red;
+  color: white;
+  border: 2px solid black;
+  border-radius: 4px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.85;
+  text-transform: uppercase;
+  cursor: pointer;
+  padding: 2px 8px;
+  font-weight: 600;
+`
